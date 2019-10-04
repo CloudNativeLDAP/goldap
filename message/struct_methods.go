0f5e4e3ee7fc8b361b7c2eb4e74ca8497d8f7995
Hiroyuki Wada <h2-wada@nri.co.jp>
2019-10-04 12:41:29 +0900

    Add helper function

--- message/struct_methods.go
+++ message/struct_methods.go
@@ -5,3 +5,10 @@ func NewLDAPMessageWithProtocolOp(po ProtocolOp) *LDAPMessage {
 	m.protocolOp = po
 	return m
 }
+
+func NewFilterEqualityMatch(desc, val string) FilterEqualityMatch {
+	return FilterEqualityMatch{
+		attributeDesc:  AttributeDescription(desc),
+		assertionValue: AssertionValue(val),
+	}
+}
